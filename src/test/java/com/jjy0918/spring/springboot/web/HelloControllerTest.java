5548a1a64597bd8965e1aa37e9328e1f5af46da9
정준영 <cua0918@naver.com>
2021-07-20 00:44:20 +0900

    chapter 2

---
+++ src/test/java/com/jjy0918/spring/springboot/web/HelloControllerTest.java
@@ -0,0 +1,46 @@
+package com.jjy0918.spring.springboot.web;
+
+import com.jjy0918.spring.springboot.web.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloReturn() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return() throws Exception{
+        String name="hello";
+        int amont=1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                .param("name",name)
+                .param("amount",String.valueOf(amont)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amont)));
+
+    }
+}
